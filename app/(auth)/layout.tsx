e9c2ba0b714626556edee612274fa3a88fd29b7a
Tomas Svojanovsky <tomas.svojanovsky33@gmail.com>
2023-12-17 20:34:51 +0100

    Add auth

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,10 +2,8 @@ import {PropsWithChildren} from "react";
 
 export default function Layout({ children }: PropsWithChildren ) {
     return (
-        <div className="flex flex-col gap-y-4">
-            <nav className="bg-red-500 w-full">
-                {children}
-            </nav>
+        <div className="h-full flex items-center justify-center">
+            {children}
         </div>
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
-import Image from 'next/image'
+import { UserButton } from '@clerk/nextjs'
 
 export default function Home() {
   return (
-    <div>Hello Twitch Clone</div>
+    <p className="flex flex-col gap-y-4">
+      <h1>Dashboard</h1>
+      <UserButton afterSignOutUrl="/"/>
+    </p>
   )
 }
