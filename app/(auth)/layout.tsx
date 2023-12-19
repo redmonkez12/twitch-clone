916fb8df31b92305cdfcfe45aca03db40a8a64a0
Tomas Svojanovsky <tomas.svojanovsky33@gmail.com>
2023-12-19 20:52:27 +0100

    Add database and dark mode

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,8 +1,11 @@
 import {PropsWithChildren} from "react";
 
+import { Logo } from "./_components/logo";
+
 export default function Layout({ children }: PropsWithChildren ) {
     return (
-        <div className="h-full flex items-center justify-center">
+        <div className="h-full flex flex-col items-center justify-center space-y-6">
+            <Logo/>
             {children}
         </div>
     );
